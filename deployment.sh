ba1db5daf76a26d012789c8a83398bfbdf649ae9
PradeepVibes <pradeep@gmail.com>
2025-04-25 14:24:08 +0530

    firstcommit

---
+++ deployment.sh
@@ -0,0 +1,3 @@
+using devops we can deploy applications for jaava applications and along
+with aws services also main thing is deployment for java applications
+
---
+++ devops.java
@@ -0,0 +1,1 @@
+using devops we can deployment for java based applications and along with ci cd pipelines creating
